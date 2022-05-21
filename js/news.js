c52c6a49e55ae279d41e56ecfe62731aaed94814
dongpham2 <dongpham21082001@gmail.com>
2022-05-21 09:07:17 +0700

    News website

---
+++ js/news.js
@@ -0,0 +1,14 @@
+// const modal = document.querySelector(".modal");
+// const closeModal = document.querySelectorAll(".icon-close");
+// const openModal = document.querySelector(".header__user");
+
+// // popup modal
+// openModal.addEventListener("click", () => {
+//   modal.classList.add("show-modal");
+// });
+
+// closeModal.forEach((e) => {
+//   e.addEventListener("click", () => {
+//     modal.classList.remove("show-modal");
+//   });
+// });
